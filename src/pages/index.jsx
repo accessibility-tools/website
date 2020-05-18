ffb41ad012734d86682fbaaecfab3acb24267f64
Eeva-Jonna Panula <eeva-jonna.panula@futurice.com>
2020-05-18 12:29:25 +0300

    Edit index-page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-function HomePage() {
-  return <div>Welcome to Next.js!</div>;
-}
+const LandingPage = () => {
+  return <div>Landing page</div>;
+};
 
-export default HomePage;
+export default LandingPage;
