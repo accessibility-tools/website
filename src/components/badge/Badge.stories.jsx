474a309c37efcef9841b1fffc981a9cc5630950e
laylapku <laylaoy@gmail.com>
2020-05-18 18:07:01 +0200

    refactored badge component with select for knobs view

--- src/components/badge/Badge.stories.jsx
+++ src/components/badge/Badge.stories.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { text, number } from "@storybook/addon-knobs";
+import { select, number } from "@storybook/addon-knobs";
 import Badge from "./Badge";
 import StoryWrapper from "../story-wrapper/StoryWrapper";
 
@@ -8,38 +8,29 @@ export default {
   component: Badge,
 };
 
-const LEVELS = {
-  CRITICAL: "critical",
-  SERIOUS: "serious",
-  MODERATE: "moderate",
-  MINOR: "minor",
+const issues = {
+  critical: 6,
+  serious: 10,
+  moderate: 3,
+  minor: 1,
 };
 
-// mock data, to be replaced with report data later
-const ISSUECOUNT = {
-  CRITICAL: 6,
-  SERIOUS: 10,
-  MODERATE: 3,
-  MINOR: 1,
-};
+const label = "level: ";
+const levels = ["critical", "serious", "moderate", "minor"];
 
-export const allBadges = () => (
-  <StoryWrapper>
-    <Badge
-      level={text("level 1", LEVELS.CRITICAL)}
-      issueCount={number(LEVELS.CRITICAL + " issues", ISSUECOUNT.CRITICAL)}
-    />
-    <Badge
-      level={text("level 2", LEVELS.SERIOUS)}
-      issueCount={number(LEVELS.SERIOUS + " issues", ISSUECOUNT.SERIOUS)}
-    />
-    <Badge
-      level={text("level 3", LEVELS.MODERATE)}
-      issueCount={number(LEVELS.MODERATE + " issues", ISSUECOUNT.MODERATE)}
-    />
-    <Badge
-      level={text("level 4", LEVELS.MINOR)}
-      issueCount={number(LEVELS.MINOR + " issues", ISSUECOUNT.MINOR)}
-    />
-  </StoryWrapper>
-);
+export const allBadges = () => {
+  console.log(Object.keys(issues));
+  return (
+    <StoryWrapper>
+      {Object.keys(issues).map((key) => {
+        return (
+          <Badge
+            key={"level name: " + key}
+            level={select(label + key, levels, key)}
+            issueCount={number(key + " issues", issues[key], { min: 0 })}
+          />
+        );
+      })}
+    </StoryWrapper>
+  );
+};
