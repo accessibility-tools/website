40dc2aa40f7acd4d19cbf4a49f2425fabb72b811
Eeva-Jonna Panula <eeva-jonna.panula@futurice.com>
2020-05-22 08:23:27 +0300

    Add SEO-component to handle sitetitle and to handle SEO-related things in the future

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,6 +6,7 @@ import ToolDetails from "../components/tool-details/ToolDetails";
 import Switcher from "../components/layout-components/Switcher";
 import Stack from "../components/layout-components/Stack";
 import Center from "../components/layout-components/Center";
+import SEO from "../components/SEO/SEO";
 
 const Index = styled(Stack)`
   background-color: ${color.extraLightPurple};
@@ -49,64 +50,67 @@ const Img = styled.img`
 
 const LandingPage = () => {
   return (
-    <Index space="extraLarge">
-      <Section>
-        <Switcher>
-          <div>
-            <HeaderWrapper>
-              <h1>Accessibility checking tools</h1>
-              <h2 className="subtitle">
-                Helping designers and developers build accessible products
-              </h2>
-            </HeaderWrapper>
-            <Center>
-              <Img src="/illustrations/start.svg" alt="" />
-            </Center>
-          </div>
-        </Switcher>
-      </Section>
-      <Section>
-        <Stack space="large">
-          <Tools>
-            <h2>The tools</h2>
-            <p>
-              Everybody should be able to use digital products regardless of
-              ability, context, or situation. Our tools can help you create
-              prodcuts with accessibility in mind.
-            </p>
-            <Button text="Learn more" isSecondary />
-          </Tools>
-          <ToolDetails title="Accessibility Web Checker" tool="webChecker" />
-        </Stack>
-      </Section>
-      <Section>
-        <Center>
-          <Stack space="medium">
-            <h2>Accessibility tools coming soon</h2>
-            <Switcher treshold="50rem" space="9rem">
-              <div>
-                <Stack space="small">
-                  <img src="/illustrations/product-ci.svg" alt="" />
-                  <h3>Command line tool</h3>
-                </Stack>
-                <Stack space="small">
-                  <img src="/illustrations/product-ios.svg" alt="" />
-                  <h3>iOS Checker</h3>
-                </Stack>
-                <Stack space="small">
-                  <img src="/illustrations/product-android.svg" alt="" />
-                  <h3>Android Checker</h3>
-                </Stack>
-              </div>
-            </Switcher>
-            <Center>
-              <Button text="View on github" icon="github" isSecondary />
-            </Center>
+    <>
+      <SEO siteTitle="Accessibility checking tools" />
+      <Index space="extraLarge">
+        <Section>
+          <Switcher>
+            <div>
+              <HeaderWrapper>
+                <h1>Accessibility checking tools</h1>
+                <h2 className="subtitle">
+                  Helping designers and developers build accessible products
+                </h2>
+              </HeaderWrapper>
+              <Center>
+                <Img src="/illustrations/start.svg" alt="" />
+              </Center>
+            </div>
+          </Switcher>
+        </Section>
+        <Section>
+          <Stack space="large">
+            <Tools>
+              <h2>The tools</h2>
+              <p>
+                Everybody should be able to use digital products regardless of
+                ability, context, or situation. Our tools can help you create
+                prodcuts with accessibility in mind.
+              </p>
+              <Button text="Learn more" isSecondary />
+            </Tools>
+            <ToolDetails title="Accessibility Web Checker" tool="webChecker" />
           </Stack>
-        </Center>
-      </Section>
-      <Section></Section>
-    </Index>
+        </Section>
+        <Section>
+          <Center>
+            <Stack space="medium">
+              <h2>Accessibility tools coming soon</h2>
+              <Switcher treshold="50rem" space="9rem">
+                <div>
+                  <Stack space="small">
+                    <img src="/illustrations/product-ci.svg" alt="" />
+                    <h3>Command line tool</h3>
+                  </Stack>
+                  <Stack space="small">
+                    <img src="/illustrations/product-ios.svg" alt="" />
+                    <h3>iOS Checker</h3>
+                  </Stack>
+                  <Stack space="small">
+                    <img src="/illustrations/product-android.svg" alt="" />
+                    <h3>Android Checker</h3>
+                  </Stack>
+                </div>
+              </Switcher>
+              <Center>
+                <Button text="View on github" icon="github" isSecondary />
+              </Center>
+            </Stack>
+          </Center>
+        </Section>
+        <Section></Section>
+      </Index>
+    </>
   );
 };
 
