2d55fccd9ff508b8a002cd1d95990095f9b4007d
Eeva-Jonna Panula <eeva-jonna.panula@futurice.com>
2020-05-22 12:06:50 +0300

    Add select to the story of ToolDetails to choose between stories

--- src/components/tool-details/ToolDetails.stories.jsx
+++ src/components/tool-details/ToolDetails.stories.jsx
@@ -2,11 +2,16 @@ import React from "react";
 import ToolDetails from "./ToolDetails";
 import StoryWrapper from "../story-wrapper/StoryWrapper";
 import Center from "../layout-components/Center";
-import { text } from "@storybook/addon-knobs";
+import { text, select } from "@storybook/addon-knobs";
 
 export default {
   title: "Design System|ToolDetails",
   component: ToolDetails,
+};
+
+const tools = {
+  "Web Checker": "webChecker",
+  "CI Tool": "ciTool",
 };
 
 export const webCheckerInfo = () => (
@@ -14,7 +19,7 @@ export const webCheckerInfo = () => (
     <Center>
       <ToolDetails
         title={text("title", "Accessibility checker")}
-        tool="webChecker"
+        tool={select("tool", tools, "webChecker")}
       />
     </Center>
   </StoryWrapper>
