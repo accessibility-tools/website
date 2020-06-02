96efdf4b5576784417f1172a37d1869a2710b9d9
laylapku <laylaoy@gmail.com>
2020-06-02 17:39:14 +0200

    add security config to CTA links

--- src/components/CTA/CTA.jsx
+++ src/components/CTA/CTA.jsx
@@ -2,21 +2,27 @@ import React from "react";
 import PropTypes from "prop-types";
 import StyledButton from "../button/Button";
 
-const CTA = ({ text, icon, href, ...otherProps }) => (
-  <StyledButton
-    as="a"
-    href={href}
-    text={text}
-    icon={icon}
-    {...otherProps}
-  ></StyledButton>
-);
+const CTA = ({ text, icon, href, isExternal, ...otherProps }) => {
+  const externalConfig = {
+    target: "_blank",
+    rel: "noopener noreferrer",
+  };
+  if (isExternal) otherProps = { ...otherProps, ...externalConfig };
+  return (
+    <StyledButton as="a" href={href} text={text} icon={icon} {...otherProps} />
+  );
+};
 
 CTA.propTypes = {
   href: PropTypes.string.isRequired,
   text: PropTypes.string.isRequired,
   icon: PropTypes.string,
   isSecondary: PropTypes.bool,
+  isExternal: PropTypes.bool,
+};
+
+CTA.defaultProps = {
+  isExternal: false,
 };
 
 export default CTA;
