da1385f9f9e4d17523dab51aafd15135fc868b6e
laylapku <laylaoy@gmail.com>
2020-06-02 17:20:47 +0200

    add knobs for CTA stories

--- src/components/CTA/CTA.jsx
+++ src/components/CTA/CTA.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import PropTypes from "prop-types";
 import StyledButton from "../button/Button";
 
-const CTA = ({ text, icon, href, ...otherProps }) => {
-  return (
-    <StyledButton as="a" href={href} text={text} icon={icon} {...otherProps} />
-  );
-};
+const CTA = ({ text, icon, href, ...otherProps }) => (
+  <StyledButton
+    as="a"
+    href={href}
+    text={text}
+    icon={icon}
+    {...otherProps}
+  ></StyledButton>
+);
 
 CTA.propTypes = {
   href: PropTypes.string.isRequired,
--- src/components/CTA/CTA.stories.jsx
+++ src/components/CTA/CTA.stories.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { text } from "@storybook/addon-knobs";
 import CTA from "./CTA";
 import StoryWrapper from "../story-wrapper/StoryWrapper";
 
@@ -16,21 +17,32 @@ const tempHref = "#";
 
 export const primary = () => (
   <StoryWrapper>
-    <CTA href={tempHref} text={texts.tool} />
-    <CTA href={tempHref} text={texts.github} />
+    <CTA href={tempHref} text={text("placeholder text", texts.tool)} />
   </StoryWrapper>
 );
 
 export const secondary = () => (
   <StoryWrapper>
-    <CTA href={tempHref} text={texts.tool} isSecondary={true} />
-    <CTA href={tempHref} text={texts.github} isSecondary={true} />
+    <CTA
+      href={tempHref}
+      text={text("placeholder text", texts.tool)}
+      isSecondary={true}
+    />
   </StoryWrapper>
 );
 
 export const withIcon = () => (
   <StoryWrapper>
-    <CTA href={tempHref} icon="github" text={texts.github} />
-    <CTA href={tempHref} icon="github" text={texts.github} isSecondary={true} />
+    <CTA
+      href={tempHref}
+      icon="github"
+      text={text("placeholder text", texts.github)}
+    />
+    <CTA
+      href={tempHref}
+      icon="github"
+      text={text("placeholder text", texts.github)}
+      isSecondary={true}
+    />
   </StoryWrapper>
 );
