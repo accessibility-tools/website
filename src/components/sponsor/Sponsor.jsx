0a47169d3b6eacad3fb160d52c2d7af18ce5b975
laylapku <laylaoy@gmail.com>
2020-06-05 14:41:34 +0200

    add link to logo and improve naming of constant in sponsor component

--- src/components/sponsor/Sponsor.jsx
+++ src/components/sponsor/Sponsor.jsx
@@ -1,43 +1,35 @@
 import React from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
-import { icons } from "../../shared/icons";
 import { spacing } from "../../shared/style";
+import Icon from "../icon/Icon";
 
-const Logo = styled.svg`
-  display: ${props => (props.block ? "block" : "inline-block")};
-  vertical-align: middle;
-  height: 1em;
+const Logo = styled(Icon)`
   max-width: 100%;
   max-height: 100%;
   margin-left: ${spacing.padding.small}px;
-
-  shape-rendering: inherit;
-  transform: translate3d(0, 0, 0);
 `;
 
-const Path = styled.path`
-  fill: ${props => props.color || "currentColor"};
-`;
-
-const ShoutOut = styled.div`
+const LogoWrapper = styled.div`
   display: inline-flex;
   align-items: baseline;
   justify-content: flex-start;
-  margin: 0 ${spacing.padding.large}px;
 `;
 
 /* expose the className prop on exported component to receive new className */
-const Sponsor = ({ block, className }) => {
-  return (
-    <ShoutOut className={className}>
-      <p>Sponsored by</p>
-      <Logo viewBox="0 0 100 21" className="Logo" block={block}>
-        <Path d={icons["FutuLogo"]} />
-      </Logo>
-    </ShoutOut>
-  );
-};
+const Sponsor = ({ block, className }) => (
+  <LogoWrapper className={className}>
+    <p>Sponsored by</p>
+    <a href="https://futurice.com/" target="__blank">
+      <Logo
+        viewBox="0 0 100 21"
+        className="Logo"
+        block={block}
+        iconPath="FutuLogo"
+      ></Logo>
+    </a>
+  </LogoWrapper>
+);
 
 Sponsor.propTypes = {
   block: PropTypes.bool,
