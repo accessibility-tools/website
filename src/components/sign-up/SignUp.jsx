b5b2f49e8ef2e6f65c403a2a35935a9e83fc0f35
laylapku <laylaoy@gmail.com>
2020-06-19 16:38:03 +0200

    sign up button styling

--- src/components/sign-up/SignUp.jsx
+++ src/components/sign-up/SignUp.jsx
@@ -1,25 +1,38 @@
 import React from "react";
+import styled from "styled-components";
 import { color } from "../../shared/style";
 import Stack from "../layout-components/Stack";
 import Center from "../layout-components/Center";
 import TextInput from "../text-input/TextInput";
 import Button from "../button/Button";
 
+const SignUpBtn = styled(Button)`
+  margin-left: auto;
+  width: -webkit-fill-available;
+
+  @media (min-width: 48rem) {
+    width: 19rem;
+  }
+`;
+
 const SignUp = () => (
   <Stack space="large" width="100%">
-    <h2>Sign up to be notified for the release</h2>
     <Center>
-      <TextInput
-        id="email"
-        label="email"
-        width={576}
-        placeholder="email@futurice.com"
-        hintText="Your e-mail will not be used for any other purposes. No spam. We promise."
-        hintIcon="manicule"
-        iconColor={color.blue}
-      />
+      <h2>Sign up to be notified for the release</h2>
     </Center>
-    <Button text="Sign Up" />
+    <Center>
+      <Stack>
+        <TextInput
+          id="email"
+          label="email"
+          placeholder="email@futurice.com"
+          hintIcon="manicule"
+          hintText="Your e-mail will not be used for any other purposes. No spam. We promise."
+          iconColor={color.blue}
+        />
+        <SignUpBtn text="Sign Up" />
+      </Stack>
+    </Center>
   </Stack>
 );
 
