6b501593394d09baa09c1eb1b135569ba70e6ce6
laylapku <laylaoy@gmail.com>
2020-06-26 16:32:44 +0200

    add imprint page and footer link

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import Link from "next/link";
 import styled from "styled-components";
 import { spacing } from "../../shared/style";
 import Icon from "../icon/Icon";
-import Link from "../links/Link";
+import StyledLink from "../links/Link";
 
 const StyledFooter = styled.footer`
   display: flex;
@@ -21,7 +22,7 @@ const LicenseWrapper = styled.div`
   }
 `;
 
-const LicenseLink = styled(Link)`
+const LicenseLink = styled(StyledLink)`
   display: inline-flex;
   align-items: center;
   & > :first-child {
@@ -57,8 +58,8 @@ const Footer = () => {
         <FullText>The tool is licensed under Creative Commons CC-BY.</FullText>
         <ShortText>CC-BY.</ShortText>
       </LicenseWrapper>
-      <Link isSecondary={true} href="/">
-        imprint
+      <Link href="/imprint">
+        <StyledLink isSecondary={true}>imprint</StyledLink>
       </Link>
     </StyledFooter>
   );
