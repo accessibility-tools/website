754b2bcc715d0e41c0ee927779c3c82bda06014a
laylapku <laylaoy@gmail.com>
2020-07-03 17:04:10 +0200

    report details card component

---
+++ src/components/report/FixElement.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+import styled from "styled-components";
+import { color } from "../../shared/style";
+import Stack from "../layout-components/Stack";
+import Link from "../links/Link";
+
+const FixContainer = styled(Stack)`
+  margin-top: 0;
+  padding: 2rem;
+
+  &:nth-child(even) {
+    background-color: ${color.extraLightPurple};
+  }
+`;
+
+const SubTitle = styled.h5`
+  max-width: none;
+`;
+
+const ElementList = styled.ul`
+  li {
+    list-style: disc;
+  }
+`;
+
+const ElementLink = styled(Link)`
+  font-size: initial;
+  align-self: flex-end;
+`;
+
+const FixElement = () => {
+  return (
+    <FixContainer>
+      <SubTitle>
+        1 element has insufficient color contrast of 2.35:1. The expected
+        contrast ratio is 3:1.
+      </SubTitle>
+      <p>Fix any of the following:</p>
+      <ElementList>
+        <li>foreground color: #0ab587</li>
+        <li>background color: #f2f2f2</li>
+        <li>font size: 45pt</li>
+        <li>font weight: bold</li>
+      </ElementList>
+      <ElementLink isExternal={true}>view element</ElementLink>
+    </FixContainer>
+  );
+};
+
+export default FixElement;
---
+++ src/components/report/ReportDetailsCard.jsx
@@ -0,0 +1,98 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import { color } from "../../shared/style";
+import Stack from "../layout-components/Stack";
+import Badge from "../badge/Badge";
+import Link from "../links/Link";
+import ArrowIcon from "../icon/ArrowIcon";
+import HiddenDetails from "./HiddenDetails";
+
+const CardContainer = styled(Stack)`
+  @media (max-width: 48rem) {
+    width: 80vw;
+  }
+`;
+
+const Details = styled.details`
+  background-color: ${color.white};
+  display: flex;
+  flex-direction: column;
+
+  width: 100%;
+  &:active {
+    background-color: ${color.purple};
+  }
+  & > * {
+    max-width: 100%;
+    padding: 2rem;
+  }
+`;
+
+const Summary = styled.summary`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  border-radius: 3px;
+
+  &::-webkit-details-marker {
+    display: none;
+  }
+
+  &:hover {
+    cursor: pointer;
+  }
+
+  & > div > p {
+    max-width: none;
+    font-weight: normal;
+  }
+`;
+
+const DetailsBadge = styled(Badge)`
+  max-width: fit-content;
+`;
+
+const ReportDetailsCard = () => {
+  const [isOpened, setOpened] = useState(false);
+  const handleOpen = event => {
+    setOpened(event.currentTarget.open);
+  };
+  return (
+    <CardContainer space="medium">
+      <DetailsBadge
+        label="critical"
+        iconName="circle"
+        iconColor={color.error}
+      />
+      <Details onToggle={handleOpen}>
+        <Summary>
+          <div>
+            <h3>Color contrast(11)</h3>
+            <p>
+              Ensure the contrast between foreground and background colors meets
+              WCAG 2 AA contrast ratio thresholds.
+            </p>
+            <Link icon="extLink" isExternal={true}>
+              Resource to solve this issue
+            </Link>
+          </div>
+          <div>
+            <ArrowIcon icon="bArrow" direction={!isOpened && "up"} />
+          </div>
+        </Summary>
+        <HiddenDetails />
+      </Details>
+    </CardContainer>
+  );
+};
+
+ReportDetailsCard.propTypes = {
+  title: PropTypes.string,
+  subtext: PropTypes.string,
+  isIssue: PropTypes.bool,
+  isGuideline: PropTypes.bool,
+  reportData: PropTypes.object,
+};
+
+export default ReportDetailsCard;
