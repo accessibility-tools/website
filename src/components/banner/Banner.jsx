64df0e242b7b8af32a14b46ce79de3fdb21ac0bc
laylapku <laylaoy@gmail.com>
2020-07-14 13:15:49 +0200

    banner mobile view minor fixes

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -5,12 +5,13 @@ import { color } from "../../shared/style";
 
 const BannerWrapper = styled.div`
   height: 40px;
-  width: fit-content;
+  width: max-content;
   padding: 0.5rem;
   border-radius: 4px;
   border: solid 1px ${color.primary};
   background-color: ${color.extraLightPurple};
   color: ${color.primary};
+  align-self: flex-start;
 `;
 
 const Banner = ({ text }) => {
