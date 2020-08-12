46ebadca346e51b45edd1812defecd15d36f0a9c
Egor Sorokin <>
2020-08-12 16:24:30 +0200

    Fix typing of icon component

--- packages/client/src/components/icon/Icon.tsx
+++ packages/client/src/components/icon/Icon.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { icons } from '../../shared/icons';
-import { IIcon, ISVG } from './types';
+import { IIcon, ISVG, SVGProps } from './types';
 
 const Svg = styled.svg<ISVG>`
   display: ${({ block }) => block ? 'block' : 'inline-block'};
@@ -21,9 +21,20 @@ const Path = styled.path`
  * - *decorative only*: for example, it illustrates a label next to it. We must ensure that it is ignored by screen readers, by setting `aria-hidden` attribute (ex: `<Icon icon="check" aria-hidden />`)
  * - *non-decorative*: it means that it delivers information. For example, an icon as only child in a button. The meaning can be obvious visually, but it must have a proper text alternative via `aria-label` for screen readers. (ex: `<Icon icon="print" aria-label="Print this document" />`)
  */
-const Icon: React.FC<IIcon> = ({ block, icon, color, ...props }) => (
-  <Svg viewBox="0 0 16 16" className="icon" block={block} {...props}>
-    <Path d={icons[icon]} color={color} />
+const Icon: React.FC<IIcon> = ({ block, icon, color, alt, ...props }) => (
+  <Svg
+    viewBox="0 0 16 16"
+    className="icon"
+    block={block}
+    aria-labelledby="svgTitle svgDesc"
+    {...props as SVGProps}
+  >
+    <title id="svgTitle">{alt}</title>
+    <desc id="svgDesc">{alt}</desc>
+    <Path
+      d={icons[icon]}
+      color={color}
+    />
   </Svg>
 );
 
--- packages/client/src/components/icon/types.ts
+++ packages/client/src/components/icon/types.ts
@@ -1,6 +1,12 @@
-export interface IIcon extends ISVG {
+import React from 'react';
+
+export type SVGProps = React.ComponentPropsWithRef<'svg'>
+
+
+export interface IIcon extends ISVG, SVGProps {
   icon: string;
   color?: string;
+  alt?: string;
 }
 
 export interface ISVG {
